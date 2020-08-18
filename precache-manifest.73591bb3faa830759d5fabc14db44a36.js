11e5906adfed47c9ee042e7c46bc8f16d096baec
Kristian Sandratama <kristian.sandratama@outlook.com>
2020-08-18 12:25:35 +0700

    Updates

---
+++ precache-manifest.73591bb3faa830759d5fabc14db44a36.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "2d9c0c4cd41fa70ba5ce84730d949e74",
+    "url": "/index.html"
+  },
+  {
+    "revision": "14b63c44ff68396786ec",
+    "url": "/static/css/main.5f361e03.chunk.css"
+  },
+  {
+    "revision": "a5339ba7f364efdf1671",
+    "url": "/static/js/2.31128ea5.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.31128ea5.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "14b63c44ff68396786ec",
+    "url": "/static/js/main.27d43ba5.chunk.js"
+  },
+  {
+    "revision": "fb42d6fa647eeccf0b7b",
+    "url": "/static/js/runtime-main.95a0c446.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/static/media/logo.5d5d9eef.svg"
+  }
+]);
